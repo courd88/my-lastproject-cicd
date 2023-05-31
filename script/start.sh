7a067a924a3af7eeca19a14e03a0f0f8c16b3401
courd88 <121409878+courd88@users.noreply.github.com>
2023-05-31 19:35:14 +0900

    Create start.sh

---
+++ script/start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="/home/ubuntu/app"
+JAR_FILE="$PROJECT_ROOT/springboot-app.jar"
+
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+echo "$TIME_NOW : copy $JAR_FILE" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/lastproject/target/*.jar $JAR_FILE
+
+echo "$TIME_NOW : run $JAR_FILE" >> $DEPLOY_LOG
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW : process id is $CURRENT_PID" >> $DEPLOY_LOG  
